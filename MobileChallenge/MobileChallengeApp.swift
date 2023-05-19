b76dc13098f7f29a107cd205aa3a2c97861affdb
Ben Comber <bfgcomber@gmail.com>
2023-05-19 18:16:30 +0100

    Initial Commit

---
+++ MobileChallenge/MobileChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileChallengeApp.swift
+//  MobileChallenge
+//
+//  Created by Ben Comber on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
